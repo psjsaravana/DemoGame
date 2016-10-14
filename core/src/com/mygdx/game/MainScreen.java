a33990853b48d0347f02100785bcaaea388f27e6
psjsaravana <psj.saravana@gmail.com>
2016-10-15 00:10:05 +0530

    walking animation added

--- core/src/com/mygdx/game/MainScreen.java
+++ core/src/com/mygdx/game/MainScreen.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mygdx.game.utils.ParallaxBackground;
 import com.mygdx.game.utils.ParallaxLayer;
 import com.mygdx.game.utils.ParallaxWidget;
@@ -16,7 +18,11 @@ import com.mygdx.game.utils.ParallaxWidget;
  */
 public class MainScreen implements Screen {
     SpriteBatch batch;
-    Texture building,backgroundImage,ground;
+    Texture building,backgroundImage,ground,walkSheet;
+    TextureRegion[] walkFrames;
+    TextureRegion currentFrame;
+    Animation walkAnimation;
+    float stateTime;
     MyGdxGame myGame;
     OrthographicCamera camera;
     int srcXBuilding,srcXGround;
@@ -25,6 +31,19 @@ public class MainScreen implements Screen {
 
     MainScreen(MyGdxGame myGdxGame) {
         myGame = myGdxGame;
+        walkSheet = new  Texture(Gdx.files.internal("zombie.png"));
+
+        TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth()/8, walkSheet.getHeight()/1);              // #10
+        walkFrames = new TextureRegion[8 * 1];
+        int index = 0;
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 8; j++) {
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+        walkAnimation = new Animation(0.25f, walkFrames);
+        stateTime = 0f;
+
         batch = new SpriteBatch();
         srcXBuilding = 0;
         srcXGround = 0;
@@ -53,6 +72,8 @@ public class MainScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stateTime += Gdx.graphics.getDeltaTime();
+        currentFrame = walkAnimation.getKeyFrame(stateTime, true);
         //camera.update();
         //background.moveX(30*delta);
         //background.render();
@@ -61,6 +82,7 @@ public class MainScreen implements Screen {
         batch.draw(backgroundImage,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         batch.draw(building,0 ,0,srcXBuilding,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(ground,0 ,0,srcXGround,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(currentFrame,50,70,currentFrame.getRegionWidth()*2,Gdx.graphics.getHeight()/3);
         srcXBuilding+=1;
         srcXGround +=2;
         Gdx.app.log("Src X value",""+srcXBuilding);
